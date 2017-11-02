fc484c73eb63809bfdb3281b4b0cee5140811b2e
Dmax071 <maximow-dimka@yandex.ru>
2017-11-02 09:05:40 +0300

    Добавленик проекта в сеть

---
+++ MinesweeperPrjct/MinesweeperPrjct/Data.asmx.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace MinesweeperPrjct
+{
+    [WebService(Namespace = "http://miniswepear.ru/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class Data : System.Web.Services.WebService
+    {
+
+        //public readonly static JSONParameters Jpar = new JSONParameters
+        //{
+        //    UseExtensions = false,
+        //    SerializeNullValues = true,
+        //    UseEscapedUnicode = false,
+        //    UseFastGuid = false,
+        //    EnableAnonymousTypes = true,
+        //    UseUTCDateTime = false
+        //};
+
+        [WebMethod(Description = "Метод добавления игрока в тоp 10")]
+        public string setPlayerTop()
+        {
+            return null;
+        }
+    }
+}
