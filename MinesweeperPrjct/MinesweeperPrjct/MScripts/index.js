89a4d963e0b60d5a5fab9853ef0ffe10ebd9a4e0
Dmax071 <maximow-dimka@yandex.ru>
2017-11-03 17:41:33 +0300

    1,1
    Картинки, класс параметры, класс поле начало написания класса клетки

--- MinesweeperPrjct/MinesweeperPrjct/MScripts/index.js
+++ MinesweeperPrjct/MinesweeperPrjct/MScripts/index.js
@@ -1,10 +1,185 @@
 ﻿$(document).ready(function () {
     init();
+
+    //событие изменения input 
     $('input').on('change keyup paste', function () {
-        alert($('#baseForm').serialize());
+        var parameters = Parameters.getParameters();
+        var gameArea = new GameArea(parameters);
+        gameArea.draArea();
     });
 
+    //   //читаем параметры 
+    //var parameters = Parameters.getParameters();
+    //  //формируем поле 
+    //var gameArea = new GameArea(parameters);
+    //// рисуем поле
+    //gameArea.draArea();
+
 })
+
+
+// формирование обьекта с параметрами игры 
+var Parameters = (function () {
+    var objParameters;
+    //получить праметры с формы 
+    function getParametersForm(formCaption) {
+        var obj = {};
+        var sendStr = $('#'+formCaption).serialize();
+        var sendArr = sendStr.split('&');
+        var obj = {};
+        sendArr.forEach((value, index, sendArr) => { //TO DO: IE
+            var elem = value.split('=');
+            obj[elem[0]] = elem[1];
+        })
+        return obj;
+    }
+
+    function createParameters() {      
+        var baseParam = getParametersForm('baseForm');
+        baseParam.cell_width = 20;
+        baseParam.cell_height = 20;
+
+        switch (baseParam.level) {
+            case '0': // easy
+                baseParam.q_width = 9;
+                baseParam.q_height = 9;
+                baseParam.q_mine = 10;
+                baseParam.cell_width = 40;
+                baseParam.cell_height = 40;
+                break
+            case '1': // medium
+                baseParam.q_width = 16;
+                baseParam.q_height = 16;
+                baseParam.q_mine = 40;
+                baseParam.cell_width = 30;
+                baseParam.cell_height = 30;
+                break
+            case '2': // hard
+                baseParam.q_width = 30;
+                baseParam.q_height = 16;
+                baseParam.q_mine = 99;
+                break;
+            default: {
+                if (baseParam.q_width <= 10) {
+                    baseParam.cell_width = 40;
+                    baseParam.cell_height = 40;
+                }
+                else if (baseParam.q_width <= 16) {
+                    baseParam.cell_width = 30
+                    baseParam.cell_height =30;
+                }
+            }
+        }
+        return baseParam;
+    }
+    return {
+        getParameters: function () {
+            //if (!objParameters) {
+                objParameters = createParameters();
+            //}
+            return objParameters;
+        }
+    };
+})();
+
+var Cell = (function () { })()
+
+/// 
+var GameArea = function (parameters) {
+    this._parameters = parameters;
+}
+
+GameArea.prototype.draArea = function () {
+    var canvas = document.querySelector('canvas')
+    var context = canvas.getContext('2d')
+
+    // Представление всех ячеек
+    var cells = []
+    var canvasWidth = this._parameters.q_width * this._parameters.cell_width;
+    var canvasHeight = this._parameters.q_height * this._parameters.cell_height;
+    canvas.width = canvasWidth
+    canvas.height = canvasHeight
+    var cellWidth = this._parameters.cell_width
+    var cellHeight = this._parameters.cell_height
+    //setParamToGameArea()
+    var cellsInRow = Math.floor(canvasWidth / cellWidth)
+    var cellsInColumn = Math.floor(canvasHeight / cellHeight)
+
+    for (var top = 0; top < canvasWidth; top += cellWidth) {
+        for (var left = 0; left < canvasHeight; left += cellHeight) {
+            var cell = {
+                top: top,
+                left: left,
+                solid: false,
+                // аргумент говорит о том каким цветом закрашивать клетку. Предполагается что у клетки может быть 2 цвета. 
+                fill: function (solid) {
+                    // запоминаем состояние закрашенности клетки
+                    this.solid = solid
+                    if (!solid) {
+                        var img = new Image();
+                        img.src = './images/1.png';
+                        context.drawImage(img, this.top, this.left, cellWidth, cellHeight);
+                    }
+                    else {
+                        context.fillStyle = '#ddd';
+                        context.fillRect(this.top, this.left, cellWidth + 1, cellHeight + 1 );
+                    }
+
+                },
+                drawBorder: function () {
+                    context.beginPath();
+                    context.strokeStyle = '#000';
+                    context.lineWidth = 1;
+
+                    // magic. According to http://stackoverflow.com/questions/8696631/canvas-drawings-like-lines-are-blurry
+                    context.strokeRect(this.top, this.left, this.top + cellHeight, this.left + cellWidth);
+
+                    //context.stroke()
+                },
+                getTop: function () {
+                    return this.top
+                },
+                getLeft: function () {
+                    return this.left
+                }
+            }
+            cells.push(cell)
+            cell.fill(true)
+            cell.drawBorder()
+        }
+    }
+
+}
+
+///
+
+var timer = 0;
+//обработчик нажатия кнопки старта игры 
+function StartGameClick() {
+    
+    // запускаем секундомер
+    // блокируем меню
+    // блокируем кнопку старт отображаем кнопки сохранить отмена
+    timer = new Date().getTime()
+}
+
+
+//обработчик нажатия кнопки отмена игры 
+function CancelGameClick() {
+}
+
+//обработчик нажатия кнопки сохранить 
+function SaveGameClick() {
+
+}
+
+//обработчик нажатия кнопки старта игры 
+function SetColorOptionClick() {
+
+
+}
+
+
 
 
 
@@ -21,39 +196,23 @@ function getFormParamToObj(formcaption) {
     return sendObj;
 }
 
-function setParamToGameArea() {
-    var cellWidth = 40; // const
-    var cellHeight = 40;// const
-
-    var obj = getFormParamToObj('baseForm');
-
-    var canvas = document.querySelector('canvas')
-    var context = canvas.getContext('2d')
-
-    canvas.width = obj.width * cellWidth;
-    canvas.height = obj.height * cellHeight
-}
-
-
-var gameArea = function () {
-
-
-}
 
 
 //главный метод
 function init() {
         var canvas = document.querySelector('canvas')
         var context = canvas.getContext('2d')
+        
+        var parameters = Parameters.getParameters();
 
         // Представление всех ячеек
         var cells = []
-        var canvasWidth = 400
-        var canvasHeight = 400
+        var canvasWidth = parameters.q_width * parameters.cell_width;
+        var canvasHeight = parameters.q_height * parameters.cell_height;
         canvas.width = canvasWidth
         canvas.height = canvasHeight
-        var cellWidth = 40
-        var cellHeight = 40
+        var cellWidth = parameters.cell_width
+        var cellHeight = parameters.cell_height
         //setParamToGameArea()
         var cellsInRow = Math.floor(canvasWidth / cellWidth)
         var cellsInColumn = Math.floor(canvasHeight / cellHeight)
@@ -68,29 +227,26 @@ function init() {
                     fill: function (solid) {
                         // запоминаем состояние закрашенности клетки
                         this.solid = solid
-                        //context.fillStyle = solid ? '#ddd' : '#fff';
-                        //context.fillRect(this.top, this.left, cellWidth, cellHeight);
                         if (!solid) {
                             var img = new Image();
-                            img.src = './images/mine.png';
+                            img.src = './images/1.png';
                             context.drawImage(img, this.top, this.left, cellWidth, cellHeight);
                         }
                         else {
-                            context.fillStyle = solid ? '#ddd' : '#fff';
-                        context.fillRect(this.top, this.left, cellWidth, cellHeight);
+                            context.fillStyle ='#ddd';
+                        context.fillRect(this.top, this.left, cellWidth-1, cellHeight-1);
                         }
 
                     },
                     drawBorder: function () {
                         context.beginPath();
                         context.strokeStyle = '#000';
+                        context.lineWidth = 1;
+
                         // magic. According to http://stackoverflow.com/questions/8696631/canvas-drawings-like-lines-are-blurry
-                        context.moveTo(this.top - 0.5, this.left - 0.5)
-                        context.lineTo(this.top - 0.5, this.left + cellWidth - 0.5)
-                        context.lineTo(this.top + cellHeight - 0.5, this.left + cellWidth - 0.5)
-                        context.lineTo(this.top + cellHeight - 0.5, this.left - 0.5)
-                        context.lineTo(this.top - 0.5, this.left - 0.5)
-                        context.stroke()
+                        context.strokeRect(this.top, this.left, this.top + cellHeight, this.left + cellWidth);
+
+                        //context.stroke()
                     },
                     getTop: function () {
                         return this.top
